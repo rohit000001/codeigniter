3acc7a4b65026e3cefa16b9e593f53f6f9e2c226
Rohit Akolkar <55943255+rohitakolkar2001@users.noreply.github.com>
2022-05-04 18:13:32 +0530

    Update rohit.sh

--- rohit.sh
+++ rohit.sh
@@ -1,14 +1,9 @@
 #!/bin/bash
-
+sudo apt update && sudo apt upgrade -y
 wget https://api.github.com/repos/codeigniter4/CodeIgniter4/zipball/v4.1.9
-
 unzip v4.1.9
-
 mv codeigniter4-CodeIgniter4-202f41a codeigniter
-
 cp -R codeigniter /var/www/html/codeigniter
-
 systemctl restart apache2.service
-
 systemctl status apache2.service
 
