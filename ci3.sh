ca3a46e88a14c5a897dc853a13b43266c87ac080
Rohit Akolkar <55943255+rohitakolkar2001@users.noreply.github.com>
2022-05-05 14:59:36 +0530

    Create ci3.sh

---
+++ ci3.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+sudo apt-get update -y
+sudo apt-get upgrade -y
+sudo apt-get install unzip
+wget https://api.github.com/repos/bcit-ci/CodeIgniter/zipball/refs/tags/3.1.13
+unzip 3.1.13
+mv bcit-ci-CodeIgniter-bcb17eb codeigniter
+cp -R codeigniter /var/www/html/codeigniter
+sudo systemctl restart apache2.service
+sudo systemctl status apache2.service
