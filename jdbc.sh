939e15964fb8883dc0fe1aef187af723b8f3fe97
Rohit Akolkar <55943255+rohitakolkar2001@users.noreply.github.com>
2022-05-09 14:57:48 +0530

    Create jdbc.sh

---
+++ jdbc.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+sudo apt-get install mysql-server                      #To install mysql server
+sudo apt-get install mysql-client                       #to install client server
+sudo apt-get install -y default-libmysqlclient-dev       #to install lib of mysql
+
+
+#sudo mysql_secure_installation
