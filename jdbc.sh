a0da7a36284d9e9361ae65ca5c1526d9ad0f96b9
Rohit Akolkar <55943255+rohitakolkar2001@users.noreply.github.com>
2022-05-09 15:18:06 +0530

    Update jdbc.sh

--- jdbc.sh
+++ jdbc.sh
@@ -4,5 +4,6 @@ sudo apt-get install mysql-server                      #To install mysql server
 sudo apt-get install mysql-client                       #to install client server
 sudo apt-get install -y default-libmysqlclient-dev       #to install lib of mysql
 
-
+#ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'qwerty123';
+#FLUSH PRIVILEGES;
 #sudo mysql_secure_installation
